050e76eb6f48afa4e5343c91638acace07596fda
GiantMing <971334483@qq.com>
2016-03-25 15:56:13 +0800

    我上午加了什么动能?我自己都忘了,好像改了一点细节

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,7 @@
 $('.song-list-wrap').on('click', function(event) {
     var target = event.target;
     if($(target).hasClass('thumb') || $(target).hasClass('thumb_num')) {
-        $(target).addClass('thumb_clicked');
+        if($(target).hasClass('thumb')) $(target).addClass('thumb_clicked');
         event.stopPropagation();
-        console.log(84793849);
-        return false;
     }
 });
