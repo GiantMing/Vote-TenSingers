9a952ba2a416bb69136989d7a4ba43bca3a6298b
GiantMing <971334483@qq.com>
2016-03-28 21:00:25 +0800

    我也不记得我两天之前干了什么

--- static/js/index.js
+++ static/js/index.js
@@ -5,3 +5,22 @@ $('.song-list-wrap').on('click', function(event) {
         event.stopPropagation();
     }
 });
+
+$(document).on('scroll', function () {
+    var windowHeight = window.innerHeight,
+        bodyHeight = document.body.offsetHeight,
+        winScrollTop = document.body.scrollTop;
+
+    var $song_fragment = $('.song_list').eq(0).clone();
+
+    if (windowHeight + winScrollTop >= bodyHeight - 10) {
+        $.post('url', {leght: $('.song_list').length}, function(res) {
+            var data = JSON.parse(res);
+            data.forEach(function(item, index, arr) {
+                $song_fragment.find('#song_link').attr('href', item.link);
+                $song_fragment.find('#singer_head').attr('src', item.singer_ehad);
+                $song_fragment.find('#song_name').text(item.song_name);
+            });
+        });
+    }
+})
--- static/js/signUp.js
+++ static/js/signUp.js
@@ -6,10 +6,17 @@ $('.weui_input').on('focus', function() {
 
 
 $('#submit-btn').on('click', function() {
+    var canSubmit = true
     $('.weui_cell').each(function(index, item) {
         if($(item).find('input').val() === '') {
+            canSubmit = false;
             $(this).addClass('weui_cell_warn');
             $(item).find('input').val('请输入');
         }
     });
+
+    if(canSubmit) {
+        $('#sign-up-form').submit();
+        
+    }
 });
